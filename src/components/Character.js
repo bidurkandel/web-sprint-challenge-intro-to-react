22f8f918fa2503e329d4be74cf0c124a00798e68
Bidur Kandel <kandelbidur11@gmail.com>
2021-06-11 16:47:49 -0500

    made some changes

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components";
+
+export default function Character({data}){
+
+    const Details = styled.ul`
+    list-style-type: none;
+    margin: 1px;
+    `;
+
+    const CharDiv = styled.div`
+    `;
+    
+    return(
+        <CharDiv>
+            <Details>
+            <li>Name: {data.name}</li>
+            <li><img src={data.image} alt="character"/></li>
+            <li>Status: {data.status}</li>
+            <li>Gender: {data.gender}</li>
+            <li>Location: {data.location.name}</li>
+            <li>Origin: {data.origin.name}</li>
+            </Details>
+        </CharDiv>
+    )
+
+
+}
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+export default{
+    primaryColor: "grey",
+    secondaryColor: "MEDIUMRED",
+    fontSize: "1.0em",
+    fontFamily: "sans-serif",
+    padding: "16px",
+}
